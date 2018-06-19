9c7ca932dae27c6effd3023f5e185cb2ce14df2e
wenzt <gsswzt@pku.edu.cn>
2018-06-19 15:11:23 +0800

    add constants.py && add net-mgmt config && add ceph public config --untested

--- plugins/gr-he-common/network/host_network.py
+++ plugins/gr-he-common/network/host_network.py
@@ -47,8 +47,8 @@ class Plugin(plugin.PluginBase):
     """
     host network configuration plugin.
     	1. enable eth VF
-    	2. set one PF eth PF to mgmt-net
-    	3. set one VF to ceph public net
+    	2. set one eth to mgmt-net
+    	3. set one eth to ceph public net
     """
 
     def __init__(self, context):
@@ -64,6 +64,39 @@ class Plugin(plugin.PluginBase):
             ohostedcons.NetworkEnv.VF_NM,
             None
         )
+        self.environment.setdefault(
+            ohostedcons.NetworkEnv.MGMT_NIC_IF,
+            None
+        )
+        self.environment.setdefault(
+            ohostedcons.NetworkEnv.MGMT_IPADDR,
+            None
+        )
+        self.environment.setdefault(
+            ohostedcons.NetworkEnv.MGMT_NETMASK,
+            '255.255.255.0'
+        )
+        self.environment.setdefault(
+            ohostedcons.NetworkEnv.MGMT_GATEWAY,
+            None
+        )
+        self.environment.setdefault(
+            ohostedcons.NetworkEnv.CEPH_NIC_IF,
+            None
+        )
+        self.environment.setdefault(
+            ohostedcons.NetworkEnv.CEPH_PUBLIC_IPADDR,
+            None
+        )
+        self.environment.setdefault(
+            ohostedcons.NetworkEnv.CEPH_PUBLIC_NETMASK,
+            '255.255.255.0'
+        )
+        self.environment.setdefault(
+            ohostedcons.NetworkEnv.CEPH_PUBLIC_GATEWAY,
+            None
+        )
+
 
     @plugin.event(
         stage = plugin.Stages.STAGE_CUSTOMIZATION,
@@ -116,3 +149,161 @@ class Plugin(plugin.PluginBase):
     	result = os.popen(cmd).read()
     	if int(result.strip()):
     	   raise RuntimeError(_('nic VF nums != 0'))
+
+    @plugin.event(
+        stage = plugin.Stages.STAGE_CUSTOMIZATION,
+        name = ohostedcons.Stages.MGMT_CFG,
+        after = (ohostedcons.Stages.ENABLE_ETHVF,),
+    )
+    def _customization(self):
+        active_ethName = ethtool.get_active_devices()
+        self.environment[
+                ohostedcons.NetworkEnv.MGMT_NIC_IF
+            ] = self.dialog.queryString(
+                name='ovehosted_mgmt_nic_if',
+                note=_(
+                    'Please indicate a nic to active Host MGMT network: (@VALUES@): '
+                ),
+                prompt=True,
+                caseSensitive=True,
+                validValues=active_ethName,
+            )
+        self.logger.info(_('indicate nis is {eth}').format(eth = self.environment[ohostedcons.NetworkEnv.MGMT_NIC_IF]))
+
+        self.environment[
+                ohostedcons.NetworkEnv.MGMT_IPADDR
+            ] = self.dialog.queryString(
+                name='ovehosted_mgmt_nic_ipaddr',
+                note=_(
+                    'Please input your Host MGMT network IPADDR: '
+                ),
+                prompt=True,
+                caseSensitive=True,
+            )
+        self.logger.info(_('ipaddr is {eth}').format(eth = self.environment[ohostedcons.NetworkEnv.MGMT_IPADDR]))
+
+        self.environment[
+                ohostedcons.NetworkEnv.MGMT_NETMASK
+            ] = self.dialog.queryString(
+                name='ovehosted_mgmt_nic_netmask',
+                note=_(
+                    'Please input your Host MGMT network METMASK [@DEFAULT@]: '
+                ),
+                prompt=True,
+                caseSensitive=True,
+                default=ohostedcons.NetworkEnv.MGMT_NETMASK,
+            )
+        self.logger.info(_('netmask is {eth}').format(eth = self.environment[ohostedcons.NetworkEnv.MGMT_NETMASK]))
+
+        self.environment[
+                ohostedcons.NetworkEnv.MGMT_GATEWAY
+            ] = self.dialog.queryString(
+                name='ovehosted_mgmt_nic_gateway',
+                note=_(
+                    'Please input your Host MGMT network GATEWAY: '
+                ),
+                prompt=True,
+                caseSensitive=True,
+            )
+        self.logger.info(_('gateway is {eth}').format(eth = self.environment[ohostedcons.NetworkEnv.MGMT_GATEWAY]))
+        eth = self.environment[ohostedcons.NetworkEnv.MGMT_NIC_IF]
+        ipaddr = self.environment[ohostedcons.NetworkEnv.MGMT_IPADDR]
+        netmask = self.environment[ohostedcons.NetworkEnv.MGMT_NETMASK]
+        gateway = self.environment[ohostedcons.NetworkEnv.MGMT_GATEWAY]
+        create_mgmt_config_file(eth, ipaddr, netmask, gateway)
+
+    @plugin.event(
+        stage = plugin.Stages.STAGE_CUSTOMIZATION,
+        name = ohostedcons.Stages.CEPH_PUBLIC_CFG,
+        after = (ohostedcons.Stages.MGMT_CFG,),
+    )
+    def _customization(self):
+        active_ethName = ethtool.get_active_devices()
+        self.environment[
+                ohostedcons.NetworkEnv.CEPH_NIC_IF
+            ] = self.dialog.queryString(
+                name='ovehosted_ceph_nic_if',
+                note=_(
+                    'Please indicate a nic to active CEPH Public network: (@VALUES@): '
+                ),
+                prompt=True,
+                caseSensitive=True,
+                validValues=active_ethName,
+            )
+        self.logger.info(_('indicate nis is {eth}').format(eth = self.environment[ohostedcons.NetworkEnv.CEPH_NIC_IF]))
+
+        self.environment[
+                ohostedcons.NetworkEnv.CEPH_PUBLIC_IPADDR
+            ] = self.dialog.queryString(
+                name='ovehosted_ceph_public_ipaddr',
+                note=_(
+                    'Please input your CEPH Public network IPADDR: '
+                ),
+                prompt=True,
+                caseSensitive=True,
+            )
+        self.logger.info(_('ipaddr is {eth}').format(eth = self.environment[ohostedcons.NetworkEnv.CEPH_PUBLIC_IPADDR]))
+
+        self.environment[
+                ohostedcons.NetworkEnv.CEPH_PUBLIC_NETMASK
+            ] = self.dialog.queryString(
+                name='ovehosted_ceph_public_netmask',
+                note=_(
+                    'Please input your CEPH Public network METMASK [@DEFAULT@]: '
+                ),
+                prompt=True,
+                caseSensitive=True,
+                default=ohostedcons.NetworkEnv.CEPH_PUBLIC_NETMASK,
+            )
+        self.logger.info(_('netmask is {eth}').format(eth = self.environment[ohostedcons.NetworkEnv.CEPH_PUBLIC_NETMASK]))
+
+        self.environment[
+                ohostedcons.NetworkEnv.CEPH_PUBLIC_GATEWAY
+            ] = self.dialog.queryString(
+                name='ovehosted_ceph_public_gateway',
+                note=_(
+                    'Please input your CEPH Public network GATEWAY: '
+                ),
+                prompt=True,
+                caseSensitive=True,
+            )
+        self.logger.info(_('gateway is {eth}').format(eth = self.environment[ohostedcons.NetworkEnv.CEPH_PUBLIC_GATEWAY]))
+        
+        eth = self.environment[ohostedcons.NetworkEnv.CEPH_NIC_IF]
+        ipaddr = self.environment[ohostedcons.NetworkEnv.CEPH_PUBLIC_IPADDR]
+        netmask = self.environment[ohostedcons.NetworkEnv.CEPH_PUBLIC_NETMASK]
+        gateway = self.environment[ohostedcons.NetworkEnv.CEPH_PUBLIC_GATEWAY]
+        create_mgmt_config_file(eth, ipaddr, netmask, gateway)
+
+
+
+
+    def create_mgmt_config_file(self, eth, ipaddr, netmask, gateway):
+        # better way ?
+        path = "/etc/sysconfig/network-scripts/ifcfg-%s" % (eth)
+        with open(path, 'w') as file:
+            file.write("TYPE=Ethernet\n")
+            file.write("BOOTPROTO=static\n")
+            file.write("DEFROUTE=yes\n")
+            file.write("IPV4_FAILURE_FATAL=no\n")
+            file.write("NAME=%s\n" % (eth))
+            file.write("DEVICE=%s\n" % (eth))
+            file.write("ONBOOT=yes\n")
+            file.write("IPADDR=%s\n" % (ipaddr))
+            file.write("NETMASK=%s\n" % (netmask))
+            if gateway:
+                file.write("GATEWAY=%s\n" % (gateway))
+        cmd = "systemctl restart network"
+        os.popen(cmd) #need stderr
+        time.sleep(5) 
+        check_mgmt_pingable(ipaddr)
+
+    def check_mgmt_pingable(self, ipaddr):
+        cmd = "ping -c3 %s" % (ipaddr)
+        result = os.popen(cmd).readlines()
+        for line in result:
+            if "Unreachable" in line:
+                raise RuntimeError(_('nic mgmt set failed!!!!!!!!!'))
+
+
+
